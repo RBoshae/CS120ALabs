a29e11a2727ede00589ff06c8091a7a7a6aa8bd2
Wanderrer <rwboshae@gmail.com>
2017-02-24 17:04:36 -0800

    Add schematic Lab 5

---
+++ Lab4/sequential_logic_design/isim/flight_attendant_call_system_bm_isim_beh.exe.sim/work/m_00000000000272655610_1656498738.c
@@ -0,0 +1,191 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/rwbos/OneDrive/Documents/GitHub/CS120ALabs/Lab4/sequential_logic_design/flight_attendant_call_system_bm.v";
+static int ng1[] = {0, 0};
+static unsigned int ng2[] = {0U, 0U};
+static unsigned int ng3[] = {1U, 0U};
+static unsigned int ng4[] = {2U, 0U};
+static unsigned int ng5[] = {3U, 0U};
+
+
+
+static void Initial_29_0(char *t0)
+{
+    char *t1;
+    char *t2;
+
+LAB0:    xsi_set_current_line(29, ng0);
+
+LAB2:    xsi_set_current_line(30, ng0);
+    t1 = ((char*)((ng1)));
+    t2 = (t0 + 1768);
+    xsi_vlogvar_assign_value(t2, t1, 0, 0, 1);
+    xsi_set_current_line(31, ng0);
+    t1 = ((char*)((ng1)));
+    t2 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t2, t1, 0, 0, 1);
+
+LAB1:    return;
+}
+
+static void Always_35_1(char *t0)
+{
+    char t4[8];
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t5;
+    char *t6;
+    char *t7;
+    int t8;
+    char *t9;
+    char *t10;
+    char *t11;
+    char *t12;
+
+LAB0:    t1 = (t0 + 3096U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(35, ng0);
+    t2 = (t0 + 3664);
+    *((int *)t2) = 1;
+    t3 = (t0 + 3128);
+    *((char **)t3) = t2;
+    *((char **)t1) = &&LAB4;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(35, ng0);
+
+LAB5:    xsi_set_current_line(36, ng0);
+    t5 = (t0 + 1368U);
+    t6 = *((char **)t5);
+    t5 = (t0 + 1208U);
+    t7 = *((char **)t5);
+    xsi_vlogtype_concat(t4, 2, 2, 2U, t7, 1, t6, 1);
+
+LAB6:    t5 = ((char*)((ng2)));
+    t8 = xsi_vlog_unsigned_case_compare(t4, 2, t5, 2);
+    if (t8 == 1)
+        goto LAB7;
+
+LAB8:    t2 = ((char*)((ng3)));
+    t8 = xsi_vlog_unsigned_case_compare(t4, 2, t2, 2);
+    if (t8 == 1)
+        goto LAB9;
+
+LAB10:    t2 = ((char*)((ng4)));
+    t8 = xsi_vlog_unsigned_case_compare(t4, 2, t2, 2);
+    if (t8 == 1)
+        goto LAB11;
+
+LAB12:    t2 = ((char*)((ng5)));
+    t8 = xsi_vlog_unsigned_case_compare(t4, 2, t2, 2);
+    if (t8 == 1)
+        goto LAB13;
+
+LAB14:
+LAB16:
+LAB15:    xsi_set_current_line(46, ng0);
+    t2 = ((char*)((ng2)));
+    t3 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
+
+LAB17:    goto LAB2;
+
+LAB7:    xsi_set_current_line(38, ng0);
+    t9 = (t0 + 1768);
+    t10 = (t9 + 56U);
+    t11 = *((char **)t10);
+    t12 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t12, t11, 0, 0, 1);
+    goto LAB17;
+
+LAB9:    xsi_set_current_line(40, ng0);
+    t3 = ((char*)((ng2)));
+    t5 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t5, t3, 0, 0, 1);
+    goto LAB17;
+
+LAB11:    xsi_set_current_line(42, ng0);
+    t3 = ((char*)((ng3)));
+    t5 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t5, t3, 0, 0, 1);
+    goto LAB17;
+
+LAB13:    xsi_set_current_line(44, ng0);
+    t3 = ((char*)((ng3)));
+    t5 = (t0 + 1928);
+    xsi_vlogvar_assign_value(t5, t3, 0, 0, 1);
+    goto LAB17;
+
+}
+
+static void Always_52_2(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+
+LAB0:    t1 = (t0 + 3344U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(52, ng0);
+    t2 = (t0 + 3680);
+    *((int *)t2) = 1;
+    t3 = (t0 + 3376);
+    *((char **)t3) = t2;
+    *((char **)t1) = &&LAB4;
+
+LAB1:    return;
+LAB4:    xsi_set_current_line(52, ng0);
+
+LAB5:    xsi_set_current_line(53, ng0);
+    t4 = (t0 + 1928);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t0 + 1768);
+    xsi_vlogvar_wait_assign_value(t7, t6, 0, 0, 1, 0LL);
+    goto LAB2;
+
+}
+
+
+extern void work_m_00000000000272655610_1656498738_init()
+{
+	static char *pe[] = {(void *)Initial_29_0,(void *)Always_35_1,(void *)Always_52_2};
+	xsi_register_didat("work_m_00000000000272655610_1656498738", "isim/flight_attendant_call_system_bm_isim_beh.exe.sim/work/m_00000000000272655610_1656498738.didat");
+	xsi_register_executes(pe);
+}
